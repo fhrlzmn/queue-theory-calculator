139959add3d707e93bd8b2a1c36a534becf0ce69
fhrlzmn <fhrlzmn26@gmail.com>
2023-01-12 17:04:55 +0700

    fix render MathJax in form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,13 @@
+import { MathJax } from 'better-react-mathjax';
+
 export default function Input({ title = '', label = 'label', change }) {
   return (
     <div>
       <label className="opacity-50">{label}</label>
       <div className="relative flex items-center">
-        <label className="absolute text-center border-r-2 w-8">{title}</label>
+        <label className="absolute text-center border-r-2 w-8">
+          <MathJax>{title}</MathJax>
+        </label>
         <input
           type="number"
           className="py-2 pl-11 border-0 bg-slate-100 rounded w-full focus:ring-2"
