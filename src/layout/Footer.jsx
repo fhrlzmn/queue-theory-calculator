2a377d663deb9a110912b047bcfc39efba7291a4
fhrlzmn <fhrlzmn26@gmail.com>
2024-04-19 19:14:06 +0700

    feat: add project link

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
     <div className="bg-white rounded-lg p-2">
-      <h1 className="text-center">by Fahrul Zaman</h1>
+      <h1 className="text-center text-slate-500">
+        Build with love by{' '}
+        <a
+          href="https://github.com/fhrlzmn"
+          target="_blank"
+          className="text-blue-400 hover:underline"
+        >
+          Fahrul Zaman
+        </a>
+      </h1>
     </div>
   );
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,22 @@
 export default function Header() {
   return (
-    <div className="bg-white rounded-lg drop-shadow-sm py-3 text-center">
+    <div className="flex justify-between items-center px-6 bg-white rounded-lg drop-shadow-sm py-3 text-center">
       <h1 className="text-2xl">Queue Theory Calculator</h1>
+      <ul className="flex justify-center gap-4">
+        <li>
+          <a
+            href="https://github.com/fhrlzmn/queue-theory-calculator"
+            target="_blank"
+          >
+            <img
+              src="/github.svg"
+              width={28}
+              alt="GitHub"
+              className="text-blue-500"
+            />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
